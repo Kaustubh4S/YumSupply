78eb342eb7ef610da13c7b5fab375c86b70d022b
Kaustubh Subhash Swami <kaustubhswami16@gmail.com>
2024-03-17 17:32:12 +0530

    Unwanted code

--- Manager/Customers.aspx.cs
+++ Manager/Customers.aspx.cs
@@ -9,8 +9,6 @@ public partial class Manager_Customers : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (string.IsNullOrEmpty(Session["UserID"].ToString()) || !(Session["RoleId"].ToString() == "1"))
-            Response.Redirect("~/Default.aspx");
     }
 
     protected void btnAdd_Click(object sender, EventArgs e)
